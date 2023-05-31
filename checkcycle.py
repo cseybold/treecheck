ae626c74b9f3d160d0b61a334752420b35ca8bf7
cseybold <134991896+cseybold@users.noreply.github.com>
2023-05-31 10:22:30 -0700

    Update checkcycle.py

--- checkcycle.py
+++ checkcycle.py
@@ -1,22 +1,25 @@
+import random
+from random import sample
+
 class Graph:
-    def __init__(self, vertices):
-        self.vertices = vertices
-        self.adj_list = {v: [] for v in vertices}
+    def __init__(self, verts):
+        self.verts = verts
+        self.adj = {v: [] for v in verts}
 
     def add_edge(self, u, v):
-        self.adj_list[u].append(v)
-        self.adj_list[v].append(u)
+        self.adj[u].append(v)
+        self.adj[v].append(u)
 
     def is_tree(self):
         visited = set()
-        parent = {v: None for v in self.vertices}
+        parent = {v: None for v in self.verts}
 
         # Check for cycles using depth-first search
-        if self.has_cycle(self.vertices[0], visited, parent):
+        if self.has_cycle(self.verts[0], visited, parent):
             return False
 
         # Check if all vertices are visited
-        if len(visited) != len(self.vertices):
+        if len(visited) != len(self.verts):
             return False
 
         return True
@@ -24,7 +27,7 @@ class Graph:
     def has_cycle(self, v, visited, parent):
         visited.add(v)
 
-        for neighbor in self.adj_list[v]:
+        for neighbor in self.adj[v]:
             if neighbor not in visited:
                 if self.has_cycle(neighbor, visited, parent):
                     return True
@@ -34,12 +37,15 @@ class Graph:
         return False
 
 
-# Example usage
-g = Graph([1, 2, 3, 4, 5])
-g.add_edge(1, 2)
-g.add_edge(1, 3)
-g.add_edge(2, 4)
-g.add_edge(3, 5)
+g = Graph([1, 2, 3, 4, 5, 6, 7, 8])
+
+for x in range(1,9):
+    v1, v2 = random.sample(range(1, 9), 2)
+    if v2 not in g.adj[v1]:
+        g.add_edge(v1, v2)
+            
+for i in range(1,9):
+    print (i, g.adj[i])
 
 if g.is_tree():
     print("The graph is a tree")
